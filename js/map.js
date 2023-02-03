6f18bb19ca346bdce84580052fe4bfbd6abfe790
Bilal Zahid <bilalzhd0@gmail.com>
2023-02-04 01:08:57 +0500

    minified the js files

---
+++ js/map.js
@@ -0,0 +1,10 @@
+// FUNCTIONING MAP IMAGE TO SHOW CARD ON HOVER
+// let circles = document.querySelectorAll(".circle");
+// circles.forEach(e => e.addEventListener("mouseover", e => {
+//   let t = e.target.getAttribute("data-city");
+//   document.getElementById(t).style.opacity = 1;
+// }))
+// circles.forEach(e => e.addEventListener("mouseleave", e => {
+//   let t = e.target.getAttribute("data-city");
+//   document.getElementById(t).style.opacity = 0
+// }));
